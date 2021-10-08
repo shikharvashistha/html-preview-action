f162d8369470d705d5d069e3648f29b08fb3b3c3
Shikhar Vashistha <51105234+shikharvashistha@users.noreply.github.com>
2021-10-08 12:23:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ async function run() {
     try {
         const html_file = core.getInput("html_file");
 
-        const {gh-pages, repo: {owner, repo}} = context;
+        const {sha, repo: {owner, repo}} = context;
 
         core.setOutput(
             "url",
-            `https://htmlpreview.github.io/?https://github.com/${owner}/${repo}/blob/${sha}/${html_file}`
+            `https://htmlpreview.github.io/?https://github.com/${owner}/${repo}/blob/gh-pages/${html_file}`
         );
     } catch (e) {
         core.setFailed(e.message);
